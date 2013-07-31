709be13e6f322a8cacd07b2851dc073e49d29b9b
Ryan Weaver <ryan@thatsquality.com>
2013-07-31 11:20:44 -0400

    Finishing my new configuration setup - it was faux code before, this should all actually work

--- app/config/security.php
+++ app/config/security.php
@@ -1,6 +1,8 @@
 <?php
 
 // this is NOT actually real code - see http://symfony.com/doc/current/book/service_container.html#importing-other-container-configuration-resources
-if ($container->getParameter('security_type') == 'ldap') {
+if ($container->getParameter('ldap')) {
+    $this->import('security_ldap.yml');
+} else {
     $this->import('security_fos_only.yml');
 }
